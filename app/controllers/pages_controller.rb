c7d034e0c6928a0500c761d327c07f5eb6f51d5c
rflores <ricardo.flores@penalolen.cl>
2024-09-30 20:46:44 -0300

    definición de vista index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def index
+    @personajes = Character.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+10.times do
+    Character.create(name:Faker::Movies::HarryPotter.character, location:Faker::Movies::HarryPotter.location, house:Faker::Movies::HarryPotter.house)
+end
